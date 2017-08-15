233266de354baf340cd11653ef0cf32aec87082b
小松原望 <nozomu@komatsubaranozomunoMacBook.local>
2017-08-15 17:49:29 +0900

    読み込んだのち、データへのアクセスをどうにかする必要がある

--- k1708onefilereadall.cs
+++ k1708onefilereadall.cs
@@ -1,18 +1,22 @@
+#pragma TextEncoding = "Shift_JIS"
+#pragma rtGlobals=3		// Use modern global access method and strict wave access.
 Function OpenMultiFiles()
     Variable aa
     OPEN/D/R/MULT=1 aa
     
     String FilePaths
     FilePaths = S_fileName  //(*)
+    print S_fileName
     
     If(Strlen(FilePaths)!=0)  //(**)
         Variable NumOfPath
         NumOfPath = ItemsInList(FilePaths, "\r")
-
-        String Path
+        String Path,datatitle
         Variable i
         For (i=0; i<NumOfPath; i+=1)
             Path = StringFromList(i, FilePaths, "\r")
+            DataTitle = path[0,strlen(path)-5]
+            print datatitle
             //ImageLoad/T=jpeg/O/G Path
              ReadFile1708_spectra(Path)
         EndFor
@@ -20,6 +24,18 @@ Function OpenMultiFiles()
 End
 
 Function ReadFile1708_spectra(Path)
-    string path
-    LoadWave/A/J/D/W/Q/E=1/K=0 path
+    string path 
+    string tablename
+    //make/O wave0
+    //make/O wave1
+    //variable data0 ,data1
+    LoadWave/A/J/D/W/Q/E=1/K=0/O path // /Jはtable作成 /Aはデフォルトのwave名
+    TableName = WinName(0,2)
+    print tablename
+    WAVE/Z X = WaveRefIndexed(TableName,0,3)
+    WAVE Y = WaveRefIndexed(TableName,1,3)
+    //Duplicate/O  wave0, path_x
+    //Duplicate/O wave1, path_y
+   // killwaves wave0,wave1
+   
 End
