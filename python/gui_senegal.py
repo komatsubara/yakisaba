2d34440cc771dde48f2cc9f52972b0493691f3c2
nozomu komatsubara <fishnkoma@gmail.com>
2018-06-28 09:11:58 +0900

    w

--- python/gui_senegal.py
+++ python/gui_senegal.py
@@ -32,14 +32,28 @@ class CsvOutput:
 		self.count=0    
 #		self.name="myname"
 		self.EditBox=tk.Entry(width=50)
+		self.EditBoxJP=tk.Entry(width=50)
+		self.EntryBoxsenegal=tk.Entry(width=50)
 		self.EditBox.pack()
-		button=tk.Button(root,text="team A", command= self.__button_func) #ボタン
+		self.EditBoxJP.pack()
+		self.EntryBoxsenegal.pack()
+		self.button=tk.Button(root,text="team A", command= self.__button_func) #ボタン
 #		button=tk.Button(root,text="test",commnand=lambda : self.__button_func())
+		button2=tk.Button(root,text="read", command= self.__button_read)
+		self.button.pack()
+		button2.pack()
 		button2=tk.Button(root,text="read", command= self.__button_read) #ボタン
-		button.pack()
+		buttonjp=tk.Button(root,text="Japan", command= self.__button_read) #ボタン
+		buttonsenegal=tk.Button(root,text="Senegal", command= self.__senegal_button) #ボタン
+		buttonjp.pack()
+		buttonsenegal.pack()
 		button2.pack()
 		root.mainloop()
-
+	def __senegal_button(self):
+		self.EntryBoxsenegal.insert(tk.END,"セネガルとは引き分け")	
+		self.EditBoxJP.insert(tk.END,"日本")
+		self.EditBoxJP.pack()
+		self.EntryBoxsenegal.pack()
 c = CsvOutput()
 #c.count=0
 c.main()
