f31114ba129023535f56e825700fd3b206ab32d7
小松原望 <nozomu@komatsubaranozomunoMacBook.local>
2018-07-01 22:26:17 +0900

    no message

--- python/12_01.py
+++ python/12_01.py
@@ -1,8 +1,19 @@
+import csv
 import random
+import numpy as np
 test=[1]                                                                            
 test2=[1]
-for cnt in range(100):
+for cnt in range(100):				#ランダムな配列を作る
 		test2[0]=random.uniform(0,1)
 		test=test+test2
-#		print(cnt)	
-print(test)
+print(2)
+
+#for cnt in len(test):
+writer=csv.writer(test)
+#for cnt in test:
+f=open("test.csv","w",encoding="UTF-8")
+for cnt in test:
+	writer.writerow(cnt)
+#	f.writerow(cnt)
+	f.close()
+
---
+++ python/12_01_02.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+reactions_in_ms=np.loadtxt("test.txt")
+print(reactions_in_ms.size)
+#print(reactions_in_ms[:20])
