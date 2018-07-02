588d9c292b6e1326ab42cc66e129a9449e16f5a2
小松原望 <nozomu@komatsubaranozomunoMacBook.local>
2018-07-02 23:41:55 +0900

    no message

--- python/12_01.py
+++ python/12_01.py
@@ -1,19 +1,37 @@
 import csv
 import random
 import numpy as np
-test=[1]                                                                            
+test=[random.uniform(0,1)]                                                                            
 test2=[1]
-for cnt in range(100):				#ランダムな配列を作る
+for cnt in range(10):				#ランダムな配列を作る
 		test2[0]=random.uniform(0,1)
 		test=test+test2
-print(2)
 
+#print(test)
+#print(str(test))
 #for cnt in len(test):
-writer=csv.writer(test)
+#tt=csv.writer(test)
 #for cnt in test:
 f=open("test.csv","w",encoding="UTF-8")
-for cnt in test:
-	writer.writerow(cnt)
+writer=csv.writer(f)
+for cnt in range(len(test)):
+	writer.writerow(str(test))
+	print(cnt)
 #	f.writerow(cnt)
-	f.close()
+#	f.close()
+print("txt")
+ff=open("test.txt","w")
+for cnt in test:
+	ff.writelines(str(cnt)+"\n")
+	print(cnt)
+ff.close
+	
+fff=open("test2.txt","w")
+fff.write("test")
+fff.close()
 
+a=[1,2,3,4]
+ffff=open("test2.txt","w")
+for x in a:
+	ffff.write(str(x)+"\n")
+ffff.close()
