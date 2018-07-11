77e60de81e872601e7a0ab26916043d322f85472
小松原望 <nozomu@komatsubaranozomunoMacBook.local>
2018-07-11 09:47:31 +0900

    gui

--- python/gui/k180710_gui.py
+++ python/gui/k180710_gui.py
@@ -1,4 +1,5 @@
 import os,sys,time
+from tkinter import ttk
 import tkinter as tk
 
 class CsvOutput:
@@ -8,9 +9,9 @@ class CsvOutput:
         self.entry()
         self.count +=1
 
-    def __button_read(self):
-        self.value=self.editbox_get_value.get()
-
+    def entry(self):
+        self.editbox_get_value.insert(tk.END,"\n"+str(self.count))
+        self.editbox_get_value.pack()
     def __button_output(self):
         self.output_value=self.editbox_get_value.get()
         self.output_filename=self.editbox_output_filename.get()
@@ -18,9 +19,6 @@ class CsvOutput:
         f.write(self.output_value)
         f.close
     
-    def entry(self):
-        self.editbox_get_value.insert=(tk.END,str(self.count))
-
     def main(self):
         self.count=0
         self.value=""
@@ -32,7 +30,10 @@ class CsvOutput:
         self.editbox_get_value=tk.Entry(width=50)
         self.editbox_get_value.pack()
         self.editbox_output_filename=tk.Entry(width=50)
+        self.editbox_output_filename.insert(tk.END,"filename")
         self.editbox_output_filename.pack()
+        frame1=ttk.Frame(root,padding=10)
+        self.output=ttk.Entry(frame1,textvariable="filename",width=30,show="*")
         button_get_value=tk.Button(root,text="get value",command=self.__button_get_value)
         button_get_value.pack()
         button_output=tk.Button(root,text="output",command=self.__button_output)
