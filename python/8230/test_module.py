c6b3cd25ec4e3b71f5afc95958ca28cfc06485ee
小松原望 <nozomu@komatsubaranozomunoMacBook.local>
2018-07-22 21:57:39 +0900

    m

--- python/8230/test_module.py
+++ python/8230/test_module.py
@@ -1,5 +1,7 @@
 #from ADCMT8230 import Adcmt8230 
 import tkinter as tk
+from tkinter import ttk
+from tkinter import *
 import random
 class Test:
     def adcmt8230_part(self):
@@ -19,12 +21,12 @@ class Csvoutput:
         f.close()
     def __button_func_adcmt(self):
         self.EditBox.insert(tk.END,str(random.randint(1,10))+"\n")
-        self.EditBox.pack()
+        self.EditBox.pack()        
     def main(self):
         root=tk.Tk()
         root.title(u"title")
         root.geometry("800x600")
-        self.EditBox=tk.Entry(width=50,texvariable=a)
+        self.EditBox=tk.Entry(width=50)
         self.EditBox.pack()
         button_adcmt=tk.Button(root,text="measure",command=self.__button_func_adcmt)
         button_adcmt.pack()
@@ -32,7 +34,34 @@ class Csvoutput:
         button_csv=tk.Button(root,text="output",command=self.__button_func_csv)
         button_csv.pack()
         root.mainloop()
+class test:
+    def __button_func_value(self):
+        self.value_entry.insert(tk.END,str(random.randint(1,10))+"\n")
+        
+    def main(self):
+        root2=tk.Tk()
+        root2.title("Test")
+        frame1=ttk.Frame(root2,padding=10)
+        frame1.grid()
+
+        label1=ttk.Label(frame1,text="value",padding=(5,2))
+        label1.grid(row=0,column=0,sticky=E)
+        label2=ttk.Label(frame1,text="label2",padding=(5,2))
+        label2.grid(row=1,column=0,sticky=E)
+        label3=ttk.Label(frame1,text="label3",padding=(5,2))
+        label3.grid(row=2,column=0,sticky=E)
+        self.value=StringVar()
+        self.value_entry=ttk.Entry(frame1,textvariable=self.value,width=30)
+        self.value_entry.grid(row=0,column=1)
+        frame2=ttk.Frame(frame1,padding=(0,5))
+        frame2.grid(row=3,column=1,sticky=W)
+        value_button=ttk.Button(frame2,text="get value",command=self.__button_func_value)
+        value_button.pack(side=LEFT)
+        
+        root2.mainloop()
+        print(self.value.get())
 #d=Test()
 #d.main()
-c=Csvoutput()
+#c=Csvoutput()
+c=test()
 c.main()
