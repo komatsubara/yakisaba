e49e166403698721f57b56464c0c8fd771901798
小松原望 <nozomu@komatsubaranozomunoMacBook.local>
2018-07-22 21:04:49 +0900

    m

--- python/8230/ADCMT8230.py
+++ python/8230/ADCMT8230.py
@@ -1,45 +1,98 @@
 """
 ADCMT8230の値を読むモジュールファイル
-引数としてレンジと測定波長,スムージングするかどうか
+引数としてレンジと測定波長,スムージングするか測定する単位を指定
 戻り値として値とエラーを返す
 """
 import visa
 #import msvcrt
-class ADCMT8230:
-    def __init__(self,wavelength,measure_range,smoothing):
-        self.wavelength=wavelength
-        self.measure_range=measure_range
-        self.smoothing=smoothing
+class Adcmt8230:
+    def __init__(self,wavelength,measure_range,smoothing,measure_unit):
+        try:
+            self.wavelength=wavelength
+            self.measure_range=measure_range
+            self.smoothing=smoothing
+            self.measure_unit=measure_unit
+        except:
+            self.wavelength=1030
+            self.measure_range="auto"
+            self.smoothing=1
+        self.measure_range_comand={"auto":"R00","20nW":"R04","200nW":"R05","2000nW":"R06","20uW":"R07","200uW":"R08","2000uW":"R09","20mW":"R10","200mW":"R11"}
+        self.measure_unit_dict={"W":"DW1","dB":"DW0"}
+            
+    def get_inst_id(self):
+        rm=visa.ResourceManager()
+        self.inst_id_list=rm.list_resources()
+#            num_of_inst=len(inst_id_list)
+#            print("number of inst : ",num_of_inst)
+        for i in self.inst_id_list:
+            self.inst_id=rm.open_resource(i)
+            self.inst_name=self.inst_id.query("*IDN?")
+#                print(inst_id, " : ",inst_name)
+            if "8230" in self.inst_name:
+                break
+        return self.inst_id
+    def set_condition(self,a): #a is self.inst_id=rm.openresource  
+        inst_id=a
+        inst_id.write(self.measure_unit_dict[self.measure_unit])   #decide measure unit
+        inst_id.write(self.measure_range_comand[self.measure_range])    #decide measure range
+        inst_id.write("WL",str(self.wavelength))                    #decide wavelength
+        inst_id.write("ST",str(self.smoothing))
+#            self.inst_id.write("BR0")
+
+    def get_value(self,a):
+        inst_id=a
+        value=inst_id.read()
+        print(value)
 
     def main(self):
-        print(self.wavelength)
-    
+        self.get_inst_id()
+        self.set_condition(self.inst_id)
+        self.get_value(self.inst_id)
 
 if __name__ =='__main__':
-    class ADCMT8230:
-        def __init__(self,wavelength,measure_range,smoothing):
+    class Adcmt8230_main:
+        def __init__(self,wavelength,measure_range,smoothing,measure_unit):
             try:
                 self.wavelength=wavelength
                 self.measure_range=measure_range
                 self.smoothing=smoothing
+                self.measure_unit=measure_unit
             except:
                 self.wavelength=1030
-                self.measure_range=20
+                self.measure_range="auto"
                 self.smoothing=1
-        def main(self):
+            self.measure_range_comand={"auto":"R00","20nW":"R04","200nW":"R05","2000nW":"R06","20uW":"R07","200uW":"R08","2000uW":"R09","20mW":"R10","200mW":"R11"}
+            self.measure_unit_dict={"W":"DW1","dB":"DW0"}
+
+        def get_inst_id(self):
             rm=visa.ResourceManager()
-            inst_id_list=rm.list_resources()
-            num_of_inst=len(inst_id_list)
+            self.inst_id_list=rm.list_resources()
+#            num_of_inst=len(inst_id_list)
+#            print("number of inst : ",num_of_inst)
+            for i in self.inst_id_list:
+                self.inst_id=rm.open_resource(i)
+                self.inst_name=self.inst_id.query("*IDN?")
+#                print(inst_id, " : ",inst_name)
+                if "8230" in self.inst_name:
+                    break
 
+        def set_condition(self,a): #a is self.inst_id=rm.openresource  
+            inst_id=a
+            inst_id.write(self.measure_unit_dict[self.measure_unit])   #decide measure unit
+            inst_id.write(self.measure_range_comand[self.measure_range])    #decide measure range
+            inst_id.write("WL",str(self.wavelength))                    #decide wavelength
+            inst_id.write("ST",str(self.smoothing))
+#            self.inst_id.write("BR0")
 
-            print("number of inst : ",num_of_inst)
-            for i in inst_id_list:
-                inst_id=rm.open_resource(i)
-                inst_name=inst_id.query("*IDN?")
-                print(inst_id, " : ",inst_name)
-#                if "8230" in inst_name:
-#                    break
-            self.value=inst_id.read()
-            print(self.value)
-    c=ADCMT8230(1,1,1)
+        def get_value(self,a):
+            inst_id=a
+            value=inst_id.read()
+            print(value)
+
+        def main(self):
+            self.get_inst_id()
+            self.set_condition(self.inst_id)
+            self.get_value(self.inst_id)
+
+    c=Adcmt8230_main(1030,"auto",0,"W")
     c.main()
--- python/8230/test_module.py
+++ python/8230/test_module.py
@@ -1,16 +1,38 @@
-from ADCMT8230 import ADCMT8230 
-
-
+#from ADCMT8230 import Adcmt8230 
+import tkinter as tk
+import random
 class Test:
     def adcmt8230_part(self):
         self.wavelength=1030
         self.measurement_range=20
         self.smoothing=1
-        self.c=ADCMT8230(self.wavelength,self.measurement_range,self.smoothing)
-        self.c.main()
+#        self.c=ADCMT8230(self.wavelength,self.measurement_range,self.smoothing)
+#        self.c.main()
 
     def main(self):
         self.adcmt8230_part()
+class Csvoutput:
+    def __button_func_csv(self):
+        self.value=self.EditBox.get()
+        f=open("test.csv","w")
+        f.write(self.value)
+        f.close()
+    def __button_func_adcmt(self):
+        self.EditBox.insert(tk.END,str(random.randint(1,10))+"\n")
+        self.EditBox.pack()
+    def main(self):
+        root=tk.Tk()
+        root.title(u"title")
+        root.geometry("800x600")
+        self.EditBox=tk.Entry(width=50,texvariable=a)
+        self.EditBox.pack()
+        button_adcmt=tk.Button(root,text="measure",command=self.__button_func_adcmt)
+        button_adcmt.pack()
 
-d=Test()
-d.main()
+        button_csv=tk.Button(root,text="output",command=self.__button_func_csv)
+        button_csv.pack()
+        root.mainloop()
+#d=Test()
+#d.main()
+c=Csvoutput()
+c.main()
